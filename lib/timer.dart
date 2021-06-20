1193c7bba22e85a2df962df3a88243ddb815eb7c
Bryan Cancel <bryan.o.cancel@gmail.com>
2021-06-20 02:43:19 -0400

    minor repair

--- lib/timer.dart
+++ lib/timer.dart
@@ -116,7 +116,7 @@ class _TimerState extends State<Timer> {
                             fit: BoxFit.contain,
                             child: MyTimerDisplay(
                               duration: passedTimeAbs,
-                              textColor: Colors.black,
+                              textColor: Colors.white,
                             ),
                           ),
                         ),
